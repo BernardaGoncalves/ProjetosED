9689dbbec66f0c306c25cd8db8184533ca8c126f
Bernarda Gonçalves <74868173+BernardaGoncalves@users.noreply.github.com>
2023-05-19 14:25:43 +0100

    Delete musicas directory

--- musicas/src/Musica.java
+++
@@ -1,9 +0,0 @@
-class Musica {
-    String titulo;
-    String artista;
-
-    public Musica(String titulo, String artista) {
-        this.titulo = titulo;
-        this.artista = artista;
-    }
-}
